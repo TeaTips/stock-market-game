e32b41891a49e34255ababca3ba3c8f051994108
Tom Penney <tpnn72@gmail.com>
2012-01-05 13:59:43 +1030

    Version 0.3 (see release notes for more details)

--- src/stock/b2kan/com/Main.java
+++ src/stock/b2kan/com/Main.java
@@ -15,12 +15,18 @@ import java.lang.Math;
 public class Main {
 	public static Random rand		= new Random();
 	public static Scanner inputScan	= new Scanner(System.in);
-	public static int input, day;
+	public static int input, day, daysLeft = 0, count = 0;
 	public static double price1, price2, price3, price4;
-	public static double version	= 0.2;
+	public static double version	= 0.3;
 	public static double money		= 100.0;
+	public static double debt		= 0.0;
+	public static boolean borrowed = false, lastChance = false;
 	public static int[] inventory1	= {0, 0, 0, 0};
 	public static double[] inventory2	= {0.0, 0.0, 0.0, 0.0};
+	public static boolean gfc		= false;
+	public static int rumor			= -1;
+	public static String yesno, thisRumor, oldRumor, rumorResult = "";
+	public static String[] rumors	= {"Derp Inc have hired a new CEO, share prices are expected to drop.", "Charity commends Herp Industries for their most recent work, share prices set to rise.", "Foo Co has been rumored to be carelessly dumping toxic waste into a nearby river, prices set to drop.", "Bar Pty Ltd released smear campaign against Foo Co, Foo Co prices expected to fall."};
 	
 	public static void main(String[] args) {
 		init();
@@ -28,14 +34,20 @@ public class Main {
 	}
 	
 	public static void menu() {
-		System.out.println("\n\nStock Market Game v"+version+"0.1\n" +
-							"You have $"+money+" in the bank.\n\n" +
-							"1. Stock Prices.\n" +
+		System.out.println("\n\nStock Market Game v"+version+"\n" +
+							"You have $"+money+" in the bank.\n");
+		if(borrowed == true) { System.out.println("You are $"+debt+" in debt"); }
+		if(rumor != -1 || thisRumor == "The rumor was true!" || thisRumor == "The rumor was false!") {
+			System.out.println("Latest News: "+thisRumor+"\n");
+			thisRumor	= "";
+		}
+		System.out.println("1. Stock Prices.\n" +
 							"2. Buy Stocks.\n" +
 							"3. Sell Stocks.\n" +
 							"4. My Shares.\n" +
 							"5. Next Day.\n" +
-							"6. Exit.");
+							"6. Loan Shark.\n" +
+							"7. Exit.");
 		if(inputScan.hasNextInt() == true) {
 			input	= inputScan.nextInt();
 		} else {
@@ -48,8 +60,10 @@ public class Main {
 			case 3: sellStock(); menu();
 			case 4: myShares(); menu();
 			case 5: nextDay(); stockPrices(); menu();
-			case 6: System.exit(0);
-			case 7: next100(); menu();
+			case 6: loanShark(); menu();
+			case 7: System.exit(0);
+			case 8: next100(); menu();
+			case 9: while(count <= 100) { rollForRumor(); count++; } count = 0; menu();
 			default: System.out.println("Please enter a valid choice."); sleep(500); menu();
 		}
 	}
@@ -192,24 +206,126 @@ public class Main {
 		price2	= fluctuate(price2);
 		price3	= fluctuate(price3);
 		price4	= fluctuate(price4);
+		
+		// If there is a rumor
+		if(rumor != -1) {
+			// Chance if the rumor was true or false
+			if(rand.nextInt(3) != 1) { // If the rumor is true
+				if(gfc == false) {
+					switch(rumor) {
+						case 0: price1	= price1 - (price1 * .3); price1	= Math.round(price1 * 100.0) / 100.0;
+						case 1: price2	= price2 + (price2 * .2); price2	= Math.round(price2 * 100.0) / 100.0;
+						case 2: price3	= price3 - (price3 * .4); price3	= Math.round(price3 * 100.0) / 100.0;
+						case 3: price3	= price3 - (price3 * .6); price3	= Math.round(price3 * 100.0) / 100.0;
+					}
+				}
+				thisRumor	= "The rumor was true!";
+				rumor		= -1; // Set to -2 because the rumor "it was true" doesn't deserve a postive number
+			} else { // If the rumor is false
+				thisRumor	= "The rumor was false!";
+				rumor		= -1; // Set to -2 for the same reason as above ^
+			}
+		} else {
+			if(thisRumor == "") {
+				thisRumor	= "";
+				rollForRumor();
+				rollForGfc();
+			} else {
+				rumor = -1;
+				thisRumor	= "";
+				rumorResult	= "";
+			}
+		}
+		
+		if(daysLeft != 0) {
+			daysLeft--;
+			debt	= debt + (debt * .05);
+		}
+		
+		if(daysLeft == 1) {
+			System.out.println("Logan will be stopping by tomorrow, make sure you have his money!");
+		}
+		if(borrowed == true && daysLeft == 0) {
+			if(money < debt) {
+				System.out.println("You don't have enough money to pay back Logan. You have one more day to pay him back.");
+				lastChance	= true;
+			} else {
+				lastChance	= false;
+				borrowed	= false;
+				money		= money - debt;
+			}
+			if(lastChance == true) {
+				if(money < debt) {
+					gameOver(1);
+				}
+			}
+		}
+	}
+	
+	public static void loanShark() {
+		System.out.println("\n\nLoan Shark\nHow much would you like to borrow from Logan the loan shark? (intrest rate of 5% per day)\n");
+		System.out.print("$");
+		input	= inputScan.nextInt();
+		System.out.println("Are you sure you wish to borrow $"+input+"? Intrest will be "+(input + (input * .05))+" per day. (y/n)");
+		yesno		= inputScan.nextLine();
+		
+		if(yesno == "y" || yesno == "Y" || yesno == "yes") {
+			debt		= input;
+			borrowed	= true;
+			System.out.println("You have been granted the money. Logan will be visiting in 5 days to get his money back.");
+			daysLeft	= 5;
+		}
 	}
 
-	public static double fluctuate(double fluctuated) {
+	public static double fluctuate(double fluctuation) {
 		int chance	= rand.nextInt(3);
 		double random	= rand.nextDouble();
 		// 0 = Stable(ish), 1 = Positive shift, 2 = Negative shift.
 		switch(chance) {
-			case 0: fluctuated	= fluctuated + (random * 3) - 3;
-			case 1: fluctuated	= fluctuated + (random * 10);
-			case 2: fluctuated	= fluctuated - (random * 5);
+			case 0: fluctuation	= fluctuation + (random * 3) - 3;
+			case 1: fluctuation	= fluctuation + (random * 10);
+			case 2: fluctuation	= fluctuation - (random * 5);
+		}
+		
+		// If we're going through a financial crisis
+		if(gfc == true) {
+			fluctuation	= fluctuation - (fluctuation * .8);
 		}
 		
 		// If things turned out really bad for the company and their share price is bad, make sure the price can't go below $0
-		if(fluctuated < 0.0) {
-			fluctuated	= 0.0;
+		if(fluctuation < 0.0) {
+			fluctuation	= 0.0;
 		}
 		
-		return (Math.round(fluctuated * 100.0) / 100.0);
+		return (Math.round(fluctuation * 100.0) / 100.0);
+	}
+	
+	public static void rollForRumor() {
+		if(rand.nextInt(4) == 1) {
+			boolean loop	= true;
+			int random	= rand.nextInt(4);
+			while(loop) {
+				if(rumors[random] == oldRumor) {
+					random	= rand.nextInt(4);
+				} else {
+					thisRumor	= rumors[random];
+					rumor	= random;
+					loop	= false;
+				}
+			}
+			
+			oldRumor	= thisRumor;
+			System.out.println("Latest News: "+thisRumor+"\n");
+		}
+		
+	}
+	
+	public static void rollForGfc() {
+		if(rand.nextInt(100) == 1) {
+			gfc			= true;
+			rumor		= -2;
+			thisRumor	= "A global financial crisis is predicted! Expect share prices to severely drop!";
+		}
 	}
 	
 	public static void next100() {
@@ -249,5 +365,16 @@ public class Main {
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public static void gameOver(int reason) {
+		System.out.println("\n\n\n\n\n\n\n\n\n" +
+							"	GAME OVER\n");
+		if(reason == 1) { // Reason 1 means they lost because they couldn't pay back Logan
+			System.out.println("You failed to pay back Logan.");
+		} else {
+			System.out.println("Final score: $"+money);
+		}
+		System.out.println("\n\n");
 	}
 }
